2617d295195b670b6af8e4ef5090c3c87b2cb06b
Jordan DuBose <jordan.dubose1@gmail.com>
2022-01-14 16:21:00 -0500

    Updates

---
+++ precache-manifest.efad824c0223589991c50b5e2e23894a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe7aa4647dc1407b24243ebf25ff714",
+    "url": "/index.html"
+  },
+  {
+    "revision": "817d7a6b8528b95cfdef",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "472b7f1171d1bd684cd3",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "817d7a6b8528b95cfdef",
+    "url": "/static/js/2.60ea06df.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.60ea06df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "472b7f1171d1bd684cd3",
+    "url": "/static/js/main.c722939f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
