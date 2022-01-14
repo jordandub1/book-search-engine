ce2a313212e45b389ebec67e2044fd6f1a094e52
Jordan DuBose <jordan.dubose1@gmail.com>
2022-01-14 14:12:13 -0500

    adding queries.js

---
+++ client/src/utils/queries.js
@@ -0,0 +1,16 @@
+import { gql } from "@apollo/client";
+
+export const GET_ME = gql`
+  query me($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        title
+      }
+    }
+  }
+`;
