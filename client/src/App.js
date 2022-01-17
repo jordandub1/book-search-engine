339737ae1e80f894f22c9b56f9a5e8495db40cf1
Jordan DuBose <jordan.dubose1@gmail.com>
2022-01-17 13:15:19 -0500

    updating app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,21 @@ import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
 
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+
   uri: "/graphql",
-  cache: new InMemoryCache(),
 });
 
 function App() {
